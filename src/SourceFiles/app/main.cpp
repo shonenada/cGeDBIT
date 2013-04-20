af209de0ed054a85a9755787cfe47055a4aed2d4
shonenada <shonenada@gmail.com>
2013-04-20 16:41:49 +0800

    Modified a main.cpp with more function, but not finished.

--- src/SourceFiles/app/main.cpp
+++ src/SourceFiles/app/main.cpp
@@ -1,38 +1,91 @@
 #include <iostream>
 #include <string>
+#include <string.h>
 #include <fstream>
 #include <ctime>
+#include "../../HeaderFiles/objects/IndexObject.h"
 #include "../../HeaderFiles/objects/DNA.h"
+#include "../../HeaderFiles/objects/RNA.h"
+#include "../../HeaderFiles/metric/Metric.h"
 #include "../../HeaderFiles/metric/DNAMetric.h"
+#include "../../HeaderFiles/metric/RNAMetric.h"
 
 using namespace std;
 
-int main(){
+int main(int argc, char* argv[]){
 
-	clock_t start,finish;
-	double totaltime;
-	start=clock();
+    string inputFile;
+    string outputFile;
+    char* type;
+    int maxSize = 100;
+    int fragmentLength = 5;
 
-	string filename = "data/arab1.con";
-	int maxSize = 100000;
-    int fragmentLength = 18;
+    for(int i=0;i<argc;i++)
+    {
+        //input filepath
+        if(!strcmp(argv[i], "-i"))
+        {
+            inputFile = string(argv[i+1]);
+        }
+        // output filepath
+        if(!strcmp(argv[i], "-o"))
+        {
+            outputFile = string(argv[i+1]);
+        }
+        // maxSize
+        if(!strcmp(argv[i], "-m"))
+        {
+            maxSize = atoi(argv[i+1]);
+            cout << maxSize << endl;
+        }
 
-	ofstream outfile("data/arab1-cpp.result", ios::out);
+        // fragment length
+        if(!strcmp(argv[i], "-f"))
+        {
+            fragmentLength = atoi(argv[i+1]);
+        }
 
-	cout << "Begin loadding file" << endl;
-	vector<shared_ptr<CDNA>> list = CDNA::loadData(filename, maxSize, fragmentLength);
-	CDNAMetric metric;
+        // type
+        if(!strcmp(argv[i], "-t"))
+        {
+            type = new char[strlen(argv[i+1]) + 1];
+            strcpy(type, argv[i]);
+        }
 
-	double dist;
-	for(int i=0;i<list.size()-1;i++){
-		dist = metric.getDistance(list[0].get(), list[i+1].get());
-		outfile << dist << ".0" <<endl;
-	}
+    }
 
-	finish=clock();
-	totaltime=(double)(finish-start)/CLOCKS_PER_SEC;
-	cout << "Execute Time: " << totaltime << " seconds" << endl;
+    clock_t start, finish;
+    double totalTime;
+    start = clock();
 
-	system("pause");
-	return 1;
+    ofstream outfile(outputFile, ios::out);
+
+    cout << "Begin loadding file: " << endl;
+
+    vector<shared_ptr<CIndexObject>>* list;
+    CMetric* metric;
+
+    if(!strcmp(type, "dna"))
+    {
+        (vector<shared_ptr<CDNA>>*) list = &(CDNA::loadData(inputFile, maxSize, fragmentLength));
+        metric = new CDNAMetric();
+    }
+
+    if(!strcmp(type, "rna"))
+    {
+        (vector<shared_ptr<CRNA>>*) list = &(CRNA::loadData(inputFile, maxSize, fragmentLength));
+        metric = new CRNAMetric();
+    }
+
+    double dist;
+    for(int i=0;i<list->size()-1;i++){
+        dist = metric->getDistance(*(list)[0].get(), *(list)[i+1].get());
+        outfile << dist << ".0" <<endl;
+    }
+
+    finish = clock();
+    totalTime = (double) (finish - start) / CLOCKS_PER_SEC;
+    cout << "Execute Time: " << totalTime << " seconds." << endl;
+
+    return 1;
 }
