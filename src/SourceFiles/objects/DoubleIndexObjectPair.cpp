40dc469d1329d79383abb6b4d83776b1f77e4255
shonenada <shonenada@gmail.com>
2013-05-05 16:54:37 +0800

    Added a lack file.

---
+++ src/SourceFiles/objects/DoubleIndexObjectPair.cpp
@@ -0,0 +1,38 @@
+#include "..\..\HeaderFiles\type\DoubleIndexObjectPair.h"
+
+CDoubleIndexObjectPair::CDoubleIndexObjectPair()
+{
+	object=0;
+}
+
+CDoubleIndexObjectPair::CDoubleIndexObjectPair(double d,CIndexObject *obj)
+{
+	dist = d;
+	object=obj;
+}
+
+CDoubleIndexObjectPair::~CDoubleIndexObjectPair()
+{
+
+}
+
+double CDoubleIndexObjectPair::getDouble()
+{
+	return dist;
+}
+
+CIndexObject* CDoubleIndexObjectPair::getObject()
+{
+	return object;
+}
+
+void CDoubleIndexObjectPair::setDouble(double value)
+{
+	dist = value;
+
+}
+
+void CDoubleIndexObjectPair::setObject(CIndexObject *obj)
+{
+	object=obj;
+}
